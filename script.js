e3bc401c73d783cd56620994434adfb9d220f20f
Jalen Moore <jalenmarquise.moore@gmail.com>
2023-01-10 16:47:36 -0600

    Added click event listener to number buttons and selected input div to display numbers.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
 const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".input");
 
 let displayValue = "0";
+
+numbers.forEach(function (number) {
+  number.addEventListener("click", function (e) {
+    if (displayValue === "0") {
+      displayValue = "";
+      display.innerText = displayValue;
+      displayValue += e.target.dataset.num;
+      display.innerText += displayValue;
+    } else {
+      display.innerText = "";
+      displayValue += e.target.dataset.num;
+      display.innerText += displayValue;
+    }
+  });
+});
